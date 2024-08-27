c8d5f64c008d18d08c9e7b9eadd3583a6836dd14
jewel109 <raihanurrahmanjewel@gmail.com>
2024-08-27 06:04:57 +0600

    postgress infra

---
+++ services/customer-service/src/core/infra/persistence/postgres/postgresInit.ts
@@ -0,0 +1,33 @@
+import Sequelize from "@sequelize/core";
+import Customer from "../../../domain/models/customer/Customer";
+import { PostgresDialect } from "@sequelize/postgres";
+import { loadEnvFile } from "process";
+import { dbName, dbPass, dbUserName, loadEnvVariables } from "../../../../utils/configUtils";
+// import { User } from "../core/domain/models/user/User";
+
+// export const sequelize = new Sequelize("mydatabase", "myuser", "mypassword", {
+//   host: 'localhost', port: 5432, dialect: "postgres",
+// })
+console.log("in InitializePostgres ", dbName)
+
+export const sequelize = new Sequelize({
+  user: dbUserName,
+  password: dbPass,
+  database: dbName,
+  dialect: PostgresDialect,
+  models: [Customer]
+})
+
+export const InitializePostgres = async () => {
+
+  try {
+    await sequelize.sync({ alter: true })
+    // await Customer.sync({ force: true })
+    console.log("Database synchronized")
+  } catch (error) {
+    console.log("unable to synchronize the db", error)
+
+  }
+}
+
+

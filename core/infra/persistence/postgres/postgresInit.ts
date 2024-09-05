15a7d729291c4f7bec3fe65077aee039ee422877
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-05 07:55:12 +0600

    postgres db setup

--- core/infra/persistence/postgres/postgresInit.ts
+++ core/infra/persistence/postgres/postgresInit.ts
@@ -4,20 +4,21 @@ import { PostgresDialect } from "@sequelize/postgres";
 import { loadEnvFile } from "process";
 import { dbName, dbPass, dbUserName, loadEnvVariables } from "../../../../utils/configUtils";
 import Product from "../../../domain/models/products/Product";
+import Order from "../../../domain/models/order/Order";
 // import { User } from "../core/domain/models/user/User";
 
 // export const sequelize = new Sequelize("mydatabase", "myuser", "mypassword", {
 //   host: 'localhost', port: 5432, dialect: "postgres",
 // })
-console.log("in InitializePostgres ", dbName)
-console.log(dbPass)
+// console.log("in InitializePostgres ", dbName)
+// console.log(dbPass)
 
 export const sequelize = new Sequelize({
   user: dbUserName,
   password: dbPass,
   database: dbName,
   dialect: PostgresDialect,
-  models: [Customer, Product]
+  models: [Customer, Product, Order]
 })
 
 export const InitializePostgres = async () => {
@@ -25,7 +26,7 @@ export const InitializePostgres = async () => {
   try {
     await sequelize.sync({ alter: true })
     // await Customer.sync({ force: true })
-    console.log("Database synchronized")
+    // console.log("Database synchronized")
   } catch (error) {
     console.log("unable to synchronize the db", error)
 

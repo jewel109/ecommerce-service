62806730235e5bf36574b50a1e4939e34332e67d
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-05 07:56:02 +0600

    product integration test

--- tests/integration/product/product.spec.ts
+++ tests/integration/product/product.spec.ts
@@ -1,13 +1,11 @@
 import request from 'supertest'
-import { url } from '../../../utils/configUtils'
-import { ProductAttributes } from '../../../core/domain/models/products/Product'
-import { STATUS_CODE_201, SUCCMSG } from '../../../utils/controllerUtils'
-import { loginCustomer, registerCustomer } from '../auth/auth.spec'
 import { CustomerAttributes } from '../../../core/domain/models/customer/Customer'
-import { sequelize } from '../../../core/infra/persistence/postgres/postgresInit'
-import { SuperTestResponse } from '../../../utils/testUtils'
+import { ProductAttributes } from '../../../core/domain/models/products/Product'
+import { url } from '../../../utils/configUtils'
+import { SUCCMSG } from '../../../utils/controllerUtils'
+import { SuperTestResponse, token } from '../../../utils/testUtils'
 
-const productData: ProductAttributes = {
+export const productData: ProductAttributes = {
   name: 'light',
   catagory: 'electronics',
   description: "light will pull out dark",
@@ -18,62 +16,67 @@ const customerData: CustomerAttributes = { email: "jewelrana@gmail.com", passwor
 
 
 export const createProductTest = async ({ name, catagory, description, price, stock }: ProductAttributes, token: string) =>
-  await request(url).post('/product').send({ name, catagory, description, price, stock }).set('Authorization', `bearer ${token}`) as SuperTestResponse
+  await request(url).post('/api/v1/product/add').send({ name, catagory, description, price, stock }).set('Authorization', `bearer ${token}`) as SuperTestResponse
 
 
 
-beforeAll(async () => {
-  await sequelize.destroyAll()
-})
 
 
 
-describe("Handling Authentication", () => {
 
-  test("should register customer", async () => {
-    const { body: { msg, status, data }, statusCode } = await registerCustomer(customerData)
+describe("Product and Authentication Handling", () => {
 
-    expect(msg).toBe("User created")
-    expect(statusCode).toBe(STATUS_CODE_201)
+  // let token = ''
+  //
+  // beforeAll(async () => {
+  //   await resetDb()
+  // })
+  //
+  // describe(" Authentication", () => {
+  //
+  //   test("should register customer", async () => {
+  //     const { body: { msg, status, data }, statusCode } = await registerCustomer(customerData)
+  //
+  //     expect(msg).toBe("User created")
+  //     expect(statusCode).toBe(STATUS_CODE_201)
+  //   })
+  //
+  //   test("should login and retrieve token", async () => {
+  //
+  //     const { body: { msg, status, data }, statusCode } = await loginCustomer(customerData)
+  //
+  //     expect(msg).toBe("Token created successfully")
+  //     expect(data).toHaveProperty("token")
+  //     token = data.token
+  //
+  //
+  //   })
+  //
+  // })
+  //
+
+  describe("Product creation and elasticSearch indexing", () => {
+
+    //  after creating a product  this product data will consume by kafka consumer and it will be indexed to Elastic search . A notification will be send to the Seller that he had created a product he can update,delete  the product
+
+
+
+
+
+
+    test("should successfully create a product", async () => {
+      const { body: { status, msg, data }, statusCode } = await createProductTest(productData, token) as SuperTestResponse
+
+      expect(msg).toBe(SUCCMSG)
+      expect(status).toBe("success")
+    })
+
+
+    test("Elastic search should index this product data", async () => {
+
+
+
+    })
+
   })
-
-})
-
-describe("Handling POST /product request should success", () => {
-
-  //  after creating a product  this product data will consume by kafka consumer and it will be indexed to Elastic search . A notification will be send to the Seller that he had created a product he can update,delete  the product
-
-  let token = ''
-
-  beforeAll(async () => {
-    const { body: { msg, status, data }, statusCode } = await loginCustomer(customerData)
-
-    expect(msg).toBe("Token created successfully")
-    expect(data).toHaveProperty("token")
-    token = data.token
-    console.log(token)
-
-  })
-
-
-
-
-
-  test("should contain success status", async () => {
-
-    const { body: { status, msg, data }, statusCode } = await createProductTest(productData, token)
-
-
-    expect(msg).toBe(SUCCMSG)
-    expect(status).toBe("success")
-  })
-
-
-
-  test("Elastic search should have indexed this product data and it will be searchable", async () => {
-
-
-
-  })
-
 })

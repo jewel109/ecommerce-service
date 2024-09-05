ed696d5bafccee69b882fef681ce563b7a98c1b7
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-05 07:56:57 +0600

    test utils

--- utils/testUtils.ts
+++ utils/testUtils.ts
@@ -1,3 +1,6 @@
+import { sequelize } from "../core/infra/persistence/postgres/postgresInit"
+import { customerData, loginCustomer, registerCustomer } from "../tests/integration/auth/auth.spec"
+import { STATUS_CODE_201 } from "./controllerUtils"
 
 
 export interface SuperTestResponse {
@@ -8,5 +11,25 @@ export interface SuperTestResponse {
   },
   statusCode: number
 }
+
+export const resetDb = async () => {
+  await sequelize.destroyAll()
+}
+export let token = '';
+
+beforeAll(async () => {
+  await resetDb();
+  const registerResponse = await registerCustomer(customerData) as SuperTestResponse;
+  expect(registerResponse.body.msg).toBe("Customer created");
+  expect(registerResponse.statusCode).toBe(STATUS_CODE_201);
+
+  const loginResponse = await loginCustomer(customerData) as SuperTestResponse;
+  expect(loginResponse.body.msg).toBe("Token created successfully");
+  expect(loginResponse.body.data).toHaveProperty("token");
+
+  token = loginResponse.body.data.token;
+});
+
+
 
 
